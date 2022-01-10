b9ee4749ffbff99d5fa322d549582e76d59939bb
Gianni <302325196@student.rocmondriaan.nl>
2022-01-10 14:36:57 +0100

    functions opdrachten van javascript toegevoegd
    functions opdrachten van javascript toegevoegd

---
+++ functions/Opdracht 1/htdocs/js/main.js
@@ -0,0 +1,19 @@
+
+
+function temperatuur() {
+    let temp = prompt('Hoe warm is het?');
+
+    if (temp <= 10) {
+        alert('Het is koud.')
+    } else if (temp > 10 && temp <= 16) {
+        document.getElementById('temp-16').innerHTML = 'Het is niet warm, je kan beter een jas aandoen. '
+    } else if (temp > 16 && temp < 24) {
+        console.log('Het is niet koud en niet warm.')
+    } else if (temp > 24 && temp < 30) {
+        document.getElementById('temp-24').innerHTML = 'Je hoeft geen jas aan.'
+    } else if (temp >= 30) {
+        alert('Je kan beter insmeren')
+    }
+}
+
+temperatuur();
